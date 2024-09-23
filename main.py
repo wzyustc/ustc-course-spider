f72117a809c09f0a03f56c210edeb1bb825ef14d
possible <144653085+wzyustc@users.noreply.github.com>
2024-09-23 18:20:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import argparse
-keys=['数学竞赛'] #这里放文件名中要有的关键字
-search='Mathematics Competition' #这里放搜索关键字
+keys=['复习'] #这里放文件名中要有的关键字
+search='大学生数学竞赛' #这里放搜索关键字
 parser = argparse.ArgumentParser(description='Ustc-course Spyder')
 parser.add_argument('token', help='token for your github account', type=str)
 args = parser.parse_args()
